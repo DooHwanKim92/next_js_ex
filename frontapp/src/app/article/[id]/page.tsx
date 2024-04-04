ef194d649c5c260d6847b144614b53621ddcb41e
DooHwanKim92 <rmfpsvldj01@naver.com>
2024-04-04 16:31:35 +0900

    ì수정, 삭제 구현해보기

--- frontapp/src/app/article/[id]/page.tsx
+++ frontapp/src/app/article/[id]/page.tsx
@@ -1,26 +1,110 @@
 'use client'
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import Article from "../page";
 
 export default function ArticleDetail() {
     
     const params = useParams();
 
+    const [clicked, setClicked] = useState(true)
+
     const [article, setArticle] = useState({})
 
-    useEffect(() => {
+    const fetchArticle = () => {
         fetch(`http://localhost:8090/api/v1/articles/${params.id}`)
                                 .then(row => row.json())
                                 .then(row => setArticle(row.data.article))
+    }
+
+    // 게시글 삭제
+    const deleteArticle = async (e) => {
+        e.preventDefault();
+        // 기존 button의 submit 기능을 막고 아래 함수를 실행시킨다.
+
+        const response = await fetch(`http://localhost:8090/api/v1/articles/${params.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(article)
+        });
+
+        if (response.ok) {
+            alert('게시물 삭제 완료.');
+            window.location.assign('http://localhost:3000/article');
+        } else {
+            alert('게시물 삭제 실패.');
+        }
+
+    }
+
+    useEffect(() => {
+        fetchArticle()
+        modifyArticle()
     }, [])
-    
+
+    const modifyArticle = () => {
+        (clicked ? setClicked(false) : setClicked(true))
+    }
     
     return (
-        <h1>
+        <>
             Article Detail{params.id} Page
             <div>
                 {article.id} / {article.title} / {article.content}
+                <button onClick={modifyArticle}>수정</button>
+                <button onClick={deleteArticle}>삭제</button>
+                    <div>
+                        {clicked ? <ArticleModify fetchArticle={fetchArticle}/> : null}
+                    </div>
             </div>
-        </h1>
+        </>
     );
+}
+
+// 게시글 수정
+function ArticleModify ({fetchArticle}) {
+
+    const params = useParams();
+
+    const [article, setArticle] = useState({title: '', content: ''})
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // 기존 button의 submit 기능을 막고 아래 함수를 실행시킨다.
+
+        const response = await fetch(`http://localhost:8090/api/v1/articles/${params.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(article)
+        });
+
+        if (response.ok) {
+            alert('게시물 수정 완료.');
+            fetchArticle()
+        } else {
+            alert('게시물 수정 실패.');
+        }
+
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setArticle({ ...article, [name]: value });
+        console.log({ ...article, [name]: value })
+    }
+
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="title" value={article.title} onChange={handleChange} />
+                <input type="text" name="content" value={article.content} onChange={handleChange}/>
+                <button type="submit">수정</button>
+            </form>
+        </>
+    )
 }
--- frontapp/src/app/article/page.tsx
+++ frontapp/src/app/article/page.tsx
@@ -4,32 +4,41 @@ import Link from "next/link"
 import { useEffect, useState } from "react"
 
 export default function Article () {
-    
+
     const [articles, setArticles] = useState([])
     
     useEffect(() => {
+        fetchArticle()
+    }, [])
+
+    const fetchArticle = () => {
         fetch("http://localhost:8090/api/v1/articles")
             .then(row => row.json())
             .then(row => setArticles(row.data.articles))
-    }, [articles])
+    }
 
     return (
         <>  
-            <ArticleForm />
+            <ArticleForm fetchArticle={fetchArticle} />
             <ul>
                 게시물 리스트
-                {articles.map((article) => <li> <Link href={`/article/${article.id}`}>{article.id}</Link> / {article.title} / {article.content}</li>)}
+                {articles.map((article) => <li> <Link href={`/article/${article.id}`}>{article.id}</Link> / {article.title}
+                                                
+                                                
+                                            </li>)}
             </ul>
         </>
     )
 }
 
-function ArticleForm () {
+// 게시글 등록
+function ArticleForm ({fetchArticle}) {
 
     const [article, setArticle] = useState({title: '', content: ''})
 
-    const handleSubmit = async  (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        // 기존 button의 submit 기능을 막고 아래 함수를 실행시킨다.
 
         const response = await fetch("http://localhost:8090/api/v1/articles", {
             method: 'POST',
@@ -41,6 +50,8 @@ function ArticleForm () {
 
         if (response.ok) {
             alert('게시물이 성공적으로 등록되었습니다.');
+            fetchArticle()
+            // 변경된 articleList 재정렬, Article()에서 매개변수로 받아온 함수
         } else {
             alert('게시물 등록에 실패했습니다.');
         }
